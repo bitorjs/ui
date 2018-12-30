b28dde0fc2433de39f8dcaa16018966491649c0d
huangzj <huangzj@192.168.8.110>
2018-12-30 15:47:24 +0800

    f

--- app/store/index.js
+++ app/store/index.js
@@ -5,6 +5,20 @@ Vue.use(Vuex);
 const store = new Vuex.Store({
   state: {
     count: 0
+  },
+  mutations: {
+    increate(state, payload) {
+      state.count++;
+    }
+  },
+  modules: {
+    a: {
+      mutations: {
+        ffa(state, payload) {
+          state.count++;
+        }
+      },
+    }
   }
 });
 Vue.prototype.$store = store;
--- inject/store.js
+++ inject/store.js
@@ -2,19 +2,19 @@ import Vuex from 'vuex'
 
 class Store extends Vuex.Store {
   constructor(options = {}) {
+    options['mutations']['SYS:SET'] = function (state, payload) {
+      state.count++;
+      console.log(payload)
+    }
     super(options);
 
-    this._mutations['SYS:SET'] = (state, payload) => {
-      // state.count++;
-      console.log(state, payload)
-    }
-
+    console.log(this)
   }
 
   setItem(key, value) {
-    // this.commit('SYS:SET')
+    this.commit('SYS:SET', key, value)
     // this.commit(type, payload, options)
-    console.log(this)
+    // console.log(this._modules['root']['state'].count++)
   }
 
   pushItem(key, value) {
