f800c098dc41ea7ab0eb5b755bf7ba5c92c110fb
huangzj <huangzj@mzftech.cn>
2019-02-15 15:32:11 +0800

    f

--- app-nginx/lib/parse.js
+++ app-nginx/lib/parse.js
@@ -4,20 +4,30 @@ const CommenteReg = /\n*\s*#.*/g;
 const NewLineReg= /\s*\n+\s*/g
 const SpaceReg = /\s+/g;
 const LocationReg = /\blocation\s[^{]*{[^}]*}/g;
-const ServerReg = /server\s{([^{}]+\s{[^{}]*})+}/g;
-const HeaderReg = /(events\s{[^}]*})|(http\s{.*{.*{.*}.*}.*})/g;
-const HttpReg = /http\s{.*{.*{.*}.*}.*}/g;
-const EventReg = /events\s{[^{}]*}/g;
+const ServerReg = /\bserver\s?{.*({[^{}]*})+}/g;///server\s?{([^{}]+\s{[^{}]*})+}/g;
+const HeaderReg = /(events\s?{[^}]*})|(http\s?{.*{.*{.*}.*}.*})/g;
+const HttpReg = /\bhttp\s?{.*{.*{.*}.*}.*}/g;
+const EventReg = /\bevents\s?{[^{}]*}/g;
 
 
 function parseSingle(recored, initObj){
-  let list = recored.match(/[^;]+/g);
+  let list = recored.match(/[^;]+/g);//(/\S+[^;]+\S*/g)
   if (list) {
     list.reduce((ret, item)=>{
       item.match(/(\S+)\s([^;]+)/);
+      // if(item.indexOf('expires 15d')>-1) debugger
       let k = RegExp.$1;
       let v = RegExp.$2;
-      ret[k] = v;
+      if(ret[k]) {
+        let old = ret[k];
+        if(Object.prototype.toString.call(old)!=='[object Array]') {
+          ret[k] = [old];
+        }
+        ret[k].push(v);
+      }
+      else {
+        ret[k] = v;
+      }
       return ret;
     }, initObj)
   }
@@ -34,19 +44,24 @@ export default () => {
     }
   };
 
-  let m = config.replace(CommenteReg, '').replace(NewLineReg, '').replace(SpaceReg, " ");
+  let m = config.replace(CommenteReg, '');
+  m = m.replace(NewLineReg, '');
+  m = m.replace(SpaceReg, " ");
+  m = m.replace(/\s*;\s*/g,';');
+  m = m.replace(/\s*{\s*/g,' {');
+  m = m.replace(/\s*}\s*/g,'}');
   let events = m.match(EventReg)[0];
   let http = m.match(HttpReg)[0]
   let http_left = http.replace(ServerReg,'');
   let header = m.replace(HeaderReg, '');
-  let servers = http.match(ServerReg)
+  let servers = http.match(ServerReg) || [];
 
   servers.map(server => {
     let serv = {},locs = {}
-    let locations = server.match(LocationReg);
+    let locations = server.match(LocationReg)||[];
 
     locations.map(str => {
-      str.match(/\blocation\s(\S+)\s((\S+)\s)?{([^{}]*)}/);
+      str.match(/\blocation\b\s?(\S+)\s((\S+)\s?)?{([^{}]*)}/);
       let arr = [];
       if (RegExp.$1) arr.push(RegExp.$1)
       if (RegExp.$3) arr.push(RegExp.$3)
@@ -58,10 +73,10 @@ export default () => {
   })
 
 
-  http_left.match(/http\s{([^{}]*)}/)
+  http_left.match(/http\s?{([^{}]*)}/)
   parseSingle(RegExp.$1, responce.http)
 
-  events.match(/events\s{([^{}]*)}/)
+  events.match(/events\s?{([^{}]*)}/)
   parseSingle(RegExp.$1, responce.events)
   parseSingle(header, responce)
 
